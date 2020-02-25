e97358ccddd70f2e27e5e3b4f1a6d019ff00a6c1
SMK <idsiemu@gmail.com>
2020-02-25 22:06:38 +0900

    login logout

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { Ionicons } from '@expo/vector-icons';
 import { AppLoading } from "expo";
 import * as Font from "expo-font";
 import { Asset } from "expo-asset";
-import { Text, View, AsyncStorage } from 'react-native';
+import { Text, View, AsyncStorage, TouchableOpacity } from 'react-native';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { persistCache } from 'apollo-cache-persist';
 import ApolloClient from "apollo-boost";
@@ -31,8 +31,8 @@ export default function App() {
         cache,
         ...apolloClientOptions
       });
-      const isLoggedIn = await AsyncStorage.getItem("isLoggenIn");
-      if(isLoggedIn === null || isLoggedIn === false){
+      const isLoggedIn = await AsyncStorage.getItem("isLoggedIn");
+      if(isLoggedIn === null || isLoggedIn === "false"){
         setIsLoggedIn(false);
       }else{
         setIsLoggedIn(true);
@@ -47,11 +47,38 @@ export default function App() {
   useEffect(() => {
     preLoad();
   }, []);
+
+  const logUserIn = async() => {
+    try {
+      await AsyncStorage.setItem("isLoggedIn", "true");
+      setIsLoggedIn(true);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const logUserOut = async() => {
+    try {
+      await AsyncStorage.setItem("isLoggedIn", "false");
+      setIsLoggedIn(false);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return loaded && client && isLoggedIn !== null ? (
     <ApolloProvider client={client}>
       <ThemeProvider theme={styles}>
-        <View>
-          {isLoggedIn === true ? <Text>I'm in</Text> : <Text>I'm out</Text>}
+        <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
+          {isLoggedIn === true ? (
+            <TouchableOpacity onPress={logUserOut}>
+              <Text>Log Out</Text>
+            </TouchableOpacity>
+           ) : (
+           <TouchableOpacity onPress={logUserIn}>
+             <Text>Log In</Text>
+            </TouchableOpacity>
+           )}
         </View>
       </ThemeProvider>
     </ApolloProvider>
