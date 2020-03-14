c233c5d3f0f24822168c077414d4c3d05c25a3eb
SMK <idsiemu@gmail.com>
2020-03-14 21:06:51 +0900

    by using useContext so we can access to auth anywhere

---
+++ AuthContext.js
@@ -0,0 +1,42 @@
+import React, {createContext,useContext, useState} from "react";
+import {AsyncStorage} from "react-native";
+
+export const AuthContext = createContext();
+
+export const AuthPorvider = ({children}) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(null);
+    const logUserIn = async() => {
+        try {
+          await AsyncStorage.setItem("isLoggedIn", "true");
+          setIsLoggedIn(true);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+    
+      const logUserOut = async() => {
+        try {
+          await AsyncStorage.setItem("isLoggedIn", "false");
+          setIsLoggedIn(false);
+        } catch (error) {
+          console.log(error)
+        }
+      };
+    return <AuthContext.Provider value={{isLoggedIn, logUserIn, logUserOut}}>{children}</AuthContext.Provider>;
+};
+
+export const useIsLoggedIn = () => {
+    const isLoggedIn = useContext(AuthContext);
+    console.log(isLoggedIn);
+    return isLoggedIn;
+};
+
+export const useLogin = () => {
+    const {logUserIn} = useContext(AuthContext);
+    return;
+};
+
+export const useLogOut = () => {
+    const {logUserOut} = useContext(AuthContext);
+    return;
+};
