bf6085799627361901455e03889ecda7665e8e57
SMK <idsiemu@gmail.com>
2020-03-22 21:19:21 +0900

    react navigation error.....

---
+++ navigation/AuthNavigation.js
@@ -0,0 +1,14 @@
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import Signup from "../screens/Auth/Signup";
+import Confirm from "../screens/Auth/Confirm";
+import Login from "../screens/Auth/Login";
+import AuthHome from "../screens/Auth/AuthHome";
+
+const AuthNavigation = createStackNavigator({
+    Signup,
+    Confirm,
+    Login,
+    AuthHome
+});
+
+export default createAppContainer(AuthNavigation);
